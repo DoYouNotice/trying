9b0c0340e2bbb903b61194f5a74f1ff661951e49
DoYouNotice <94945611+DoYouNotice@users.noreply.github.com>
2023-11-25 17:49:41 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,11 +18,14 @@ if response.status_code == 200:
     # Extract and print the links
     links = [link['href'] for link in link_elements]
 
+    # Use a relative path for the output file
+    output_path = os.path.join(os.getcwd(), "scraped_links.txt")
+
     # Save the links to a file
-    with open("trying\scraped_links.txt", "w") as file:
+    with open(output_path, "w") as file:
         for link in links:
             file.write(link + "\n")
 
-    print("Scraped links saved to 'scraped_links.txt'")
+    print(f"Scraped links saved to '{output_path}'")
 else:
     print(f"Failed to retrieve the page. Status code: {response.status_code}")
